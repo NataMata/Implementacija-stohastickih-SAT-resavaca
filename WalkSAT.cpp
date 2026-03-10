f11ab97e2485d11fcae9354fefa2541436cce24b
NataMata <natafil20@gmail.com>
2026-03-10 23:04:41 +0100

    Add files via upload

---
+++ WalkSAT.cpp
@@ -0,0 +1,69 @@
+#include "WalkSAT.h"
+#include <optional>
+#include <iostream>
+
+          
+uniform_real_distribution<> dis(0.0, 1.0); 
+
+optional<Valuation> WalkSAT(NormalForm& SAT_instance, int condition, double probability){
+
+	Valuation valuation = generate_random_valuation(SAT_instance);
+	Variable var_to_flip;
+
+	while(condition > 0){
+	
+		if(is_satisfiable(SAT_instance, valuation)){
+			return valuation;
+		}
+		
+		vector<Clause> unsatisfied_clauses = find_unsatisfied_clauses(SAT_instance, valuation);
+		
+		Clause unsat_clause = choose_random_unsatisified_clause(unsatisfied_clauses);
+		
+		map<Variable, int> breakcounts;
+		for(auto literal : unsat_clause){
+			int var = abs(literal);
+			breakcounts[var] = calculate_breakcount(SAT_instance, var, valuation);
+		}
+		
+		if(zero_breakcount_variables(breakcounts)){
+			var_to_flip = choose_random_zero_breakcount_variable__to_flip(breakcounts);
+		} else{
+			double p = dis(gen);
+			
+			if(p < probability){
+				var_to_flip = choose_random_variable_to_flip(unsat_clause);
+			} else{
+				auto it = min_element(breakcounts.begin(), breakcounts.end(), [](const auto& p1, const auto& p2) { return p1.second < p2.second;});
+    				var_to_flip = it->first;	
+			}
+		}
+		
+		valuation[var_to_flip] = !valuation[var_to_flip];
+		condition--;
+	}
+	
+	return nullopt;
+}
+
+int main(){
+
+	double probability = 0.4;
+
+	//NormalForm formula = {{1, 2, 3}, {1, 2, -3}, {1, -2, 3}, {1, -2, -3}, {-1, 2, 3}, {-1, 2, -3}, {-1, -2, 3}, {-1, -2, -3}};
+	
+	NormalForm formula = {
+	    {1, 2, 3}, {-1, -2, 4}, {1, -3, 5}, {-2, 3, -4}, {2, 4, -5},
+	    {-1, 3, 5}, {1, -4, -5}, {-3, 4, 5}, {2, -3, -5}, {-1, -2, 3},
+	    {1, 4, -5}, {-2, -4, 5}, {3, -4, -5}, {-1, 3, -4}, {2, 3, 4},
+	    {1, 5, -2}, {-3, -5, 2}, {4, -1, -3}, {-5, 1, 4}, {2, -4, 6}
+	};
+	
+	if(WalkSAT(formula, 10, probability)){
+		cout << "SAT" << endl;
+	} else{
+		cout << "Unknown" << endl;
+	}
+
+	return 0;
+}
---
+++ WalkSAT.h
@@ -0,0 +1,173 @@
+#ifndef WALKSAT_H
+#define WALKSAT_H
+
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <random>
+
+using namespace std;
+
+using Variable = int;
+using Literal = int;
+
+using Clause = vector<Literal>;
+using NormalForm = vector<Clause>;
+
+using Valuation = map<Variable, bool>;
+
+random_device rd;              
+mt19937 gen(rd());   
+
+set<Variable> getVariables(NormalForm& SAT_instance){
+
+	set<Variable> variables;
+	
+	for(const auto& clause : SAT_instance){
+		for(auto literal : clause){
+			if(variables.find(abs(literal)) == variables.end()){
+				variables.insert(abs(literal));
+			}
+		}
+	}
+	
+	return variables;
+
+}
+
+Valuation generate_random_valuation(NormalForm& SAT_instance){
+	
+	set<Variable> variables = getVariables(SAT_instance);
+	uniform_int_distribution<> dis(0, 1);
+	
+	Valuation valuation;
+
+	for(const auto& variable : variables){	
+		int p = dis(gen);
+		valuation[variable] = p;
+	}
+	
+	return valuation;
+}
+
+bool is_satisfiable(NormalForm& SAT_instance, Valuation& valuation){
+
+	bool formula = true;
+	for(const auto& clause : SAT_instance){
+		
+		bool clause_value = false;
+		for(auto literal : clause){
+			if(valuation[abs(literal)] == (literal > 0)){
+				clause_value = true;
+				break;
+			}
+		}
+		
+		if(!clause_value){
+			return false;
+		}
+	}
+	
+	return true;
+	
+}
+
+vector<Clause> find_unsatisfied_clauses(NormalForm& SAT_instance, Valuation& valuation){
+
+	vector<Clause> unsat_clauses;
+
+	for(const auto& clause : SAT_instance){
+	
+		bool clause_value = false;
+		for(auto literal : clause){
+			if(valuation[abs(literal)] == (literal > 0)){
+				clause_value = true;
+				break;
+			}
+		}
+		
+		if(!clause_value){
+			unsat_clauses.push_back(clause);
+		}
+	}
+
+
+	return unsat_clauses;
+}
+
+Clause choose_random_unsatisified_clause(vector<Clause>& unsatisfied_clauses){
+	
+	int array_size = unsatisfied_clauses.size();
+	
+	uniform_int_distribution<> dis(0, array_size - 1);
+	int p = dis(gen);
+	
+	return unsatisfied_clauses[p];
+	
+}
+
+int calculate_breakcount(NormalForm& SAT_instance, Variable var, Valuation& valuation){
+	
+	valuation[var] = !valuation[var];
+	int count = 0;
+	
+	for(const auto& clause : SAT_instance){
+		bool clause_value = false;
+		for(auto literal : clause){
+			if(valuation[abs(literal)] == (literal > 0)){
+				clause_value = true;
+				break;
+			}
+		}
+		
+		if(!clause_value){
+			count++;
+		}
+	}
+	
+	valuation[var] = !valuation[var];
+	
+	return count;
+}
+
+bool zero_breakcount_variables(map<Variable, int>& breakcounts){
+
+	for(const auto& el : breakcounts){
+	 	if(el.second == 0){
+			return true;
+		}
+	}
+	
+	return false;
+
+}
+
+Variable choose_random_zero_breakcount_variable__to_flip(map<Variable, int>& breakcounts){
+		
+	vector<Variable> zeros;
+	for(const auto& el : breakcounts){
+		if(el.second == 0){
+			zeros.push_back(el.first);
+		}
+	}
+	
+	int array_size = zeros.size();
+	
+	uniform_int_distribution<> dis(0, array_size - 1);
+	int p = dis(gen);
+
+	return zeros[p];
+}
+
+Variable choose_random_variable_to_flip(Clause& unsat_clause){
+	
+	int array_size = unsat_clause.size();
+	
+	uniform_int_distribution<> dis(0, array_size - 1);
+	int p = dis(gen);
+
+	return abs(unsat_clause[p]);
+}
+
+#endif
